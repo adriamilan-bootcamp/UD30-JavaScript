02a23e4ce9a1bbcb230121f1867c13e2eccd582f
Adrià Milan <adria4milan@gmail.com>
2022-09-05 00:18:25 +0200

    Ejercicio 03

---
+++ Ejercicio03/js/main.js
@@ -0,0 +1,19 @@
+
+meses = [
+    "enero",
+    "febrero",
+    "marzo",
+    "abril",
+    "mayo",
+    "junio",
+    "julio",
+    "agosto",
+    "septiembre",
+    "octubre",
+    "noviembre",
+    "diciembre"
+]
+
+meses.forEach(mes => {
+   console.log(mes) 
+});
